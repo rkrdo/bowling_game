b6418549984f7d6adb695dbe16f3656085697d7b
Ricardo Cruz <ricardocruz.a89@gmail.com>
2014-12-11 03:56:53 +0000

    Added first test case to Frame#get_score

---
+++ lib/frame.rb
@@ -0,0 +1,6 @@
+class Frame
+
+  def get_score
+    0
+  end
+end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -1,3 +1,16 @@
+require 'frame'
+require 'minitest/autorun'
+require 'minitest-spec-context'
+
 describe Frame do
+
+  describe "#get_score" do
+    context "with no throws" do
+      it 'returns a score of 0' do
+        f = Frame.new
+        f.get_score.must_equal 0
+      end
+    end
+  end
 
 end
