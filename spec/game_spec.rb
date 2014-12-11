f63c62a47413826a8ee6d89a7c21fc4a33239550
Ricardo Cruz <ricardocruz.a89@gmail.com>
2014-12-11 04:31:57 +0000

    added test case for one throw for Game#score

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,4 +2,17 @@ require 'game'
 require 'spec_helper'
 
 describe Game do
+
+  describe "#get_score" do
+
+    context "with one throw" do
+      it 'returns the throws result' do
+        g = Game.new
+        g.add 5
+        g.score.must_equal 5
+      end
+    end
+
+  end
+
 end
