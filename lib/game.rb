edb052fe88fb29cc22647ec33d261647050b7a5f
Ricardo Cruz <ricardocruz.a89@gmail.com>
2014-12-11 05:29:15 +0000

    Test case for Game#score_for_frame passing

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,8 @@ class Game
 
   def initialize
     @score = 0
+    @throws = Array.new(21)
+    @current_throw = 0
   end
 
   def score
@@ -9,10 +11,19 @@ class Game
   end
 
   def add(pins)
+    @throws[@current_throw] = pins
+    @current_throw += 1
     @score += pins
   end
 
-  def score_for_frame(frame)
-    0
+  def score_for_frame(_frame)
+    score = 0
+    ball = 0
+    while _frame > 0 && (ball < @current_throw) do
+      score += @throws[ball] + @throws[ball + 1]
+      ball += 2
+      _frame -= 1
+    end
+    score
   end
 end
