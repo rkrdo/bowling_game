65efabbb2b6e13229f0764963ee2bd0163d89475
Ricardo Cruz <ricardo.cruz@rallyhood.com>
2014-12-18 17:16:20 -0600

    Updated Game spec to use a subject

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,23 +2,22 @@ require 'game'
 require 'spec_helper'
 
 describe Game do
+  subject { Game.new }
 
   describe "#get_score" do
 
     context "with one throw" do
       it 'returns the throws result' do
-        g = Game.new
-        g.add 5
-        g.score.must_equal 5
+        subject.add 5
+        subject.score.must_equal 5
       end
     end
 
     context "with two throws" do
       it 'returns the correct result' do
-        g = Game.new
-        g.add 5
-        g.add 4
-        g.score.must_equal 9
+        subject.add 5
+        subject.add 4
+        subject.score.must_equal 9
       end
     end
 
@@ -27,14 +26,13 @@ describe Game do
   describe "#score_for_frame" do
     context "4 throws" do
       it 'returns the score of the requested frame' do
-        g = Game.new
-        g.add 5
-        g.add 4
-        g.add 7
-        g.add 2
-        g.score.must_equal 18
-        g.score_for_frame(1).must_equal 9
-        g.score_for_frame(2).must_equal 18
+        subject.add 5
+        subject.add 4
+        subject.add 7
+        subject.add 2
+        subject.score.must_equal 18
+        subject.score_for_frame(1).must_equal 9
+        subject.score_for_frame(2).must_equal 18
       end
     end
   end
