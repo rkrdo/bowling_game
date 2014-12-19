2aa494b130b954c5443506acaeebc55eb434aa8b
Ricardo Cruz <ricardocruz.a89@gmail.com>
2014-12-19 05:20:58 +0000

    Renamed game tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,46 +4,39 @@ require 'game'
 describe Game do
   subject { Game.new }
 
-  describe "#get_score" do
-
-    context "with two throws" do
-      it 'returns the correct result' do
-        subject.add 5
-        subject.add 4
-        subject.score.must_equal 9
-        subject.current_frame.must_equal 2
-      end
+  context "with two throws" do
+    it 'returns the correct result' do
+      subject.add 5
+      subject.add 4
+      subject.score.must_equal 9
+      subject.current_frame.must_equal 2
     end
-
   end
 
-  describe "#score_for_frame" do
-    context "4 throws" do
-      it 'returns the score of the requested frame' do
-        subject.add 5
-        subject.add 4
-        subject.add 7
-        subject.add 2
-        subject.score.must_equal 18
-        subject.score_for_frame(1).must_equal 9
-        subject.score_for_frame(2).must_equal 18
-        subject.current_frame.must_equal 3
-      end
+  context "4 throws" do
+    it 'returns the score of the requested frame' do
+      subject.add 5
+      subject.add 4
+      subject.add 7
+      subject.add 2
+      subject.score.must_equal 18
+      subject.score_for_frame(1).must_equal 9
+      subject.score_for_frame(2).must_equal 18
+      subject.current_frame.must_equal 3
     end
-
-    context "scoring a spare" do
-      it 'returns the correct score' do
-        subject.add 3
-        subject.add 7
-        subject.add 3
-        subject.score_for_frame(1).must_equal 13
-        subject.current_frame.must_equal 2
-      end
-    end
-
   end
 
-  describe "score of frame after a spare" do
+  context "scoring a spare" do
+    it 'returns the correct score' do
+      subject.add 3
+      subject.add 7
+      subject.add 3
+      subject.score_for_frame(1).must_equal 13
+      subject.current_frame.must_equal 2
+    end
+  end
+
+  context "score of frame after a spare" do
     it 'returns the correct score' do
       subject.add 3
       subject.add 7
