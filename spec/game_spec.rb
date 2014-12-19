ec36bea0d645080bb8d520a9a49838d8ebeea417
Ricardo Cruz <ricardo.cruz@rallyhood.com>
2014-12-19 16:38:00 -0600

    changed tests to have a similar name as the ones from the book

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,78 +4,64 @@ require 'game'
 describe Game do
   let(:g) { Game.new }
 
-  context "with two throws" do
-    it 'returns the correct result' do
-      g.add 5
-      g.add 4
-      g.score.must_equal 9
-      g.current_frame.must_equal 2
-    end
+  it 'two throws no mark' do
+    g.add 5
+    g.add 4
+    g.score.must_equal 9
+    g.current_frame.must_equal 2
   end
 
-  context "4 throws" do
-    it 'returns the score of the requested frame' do
-      g.add 5
-      g.add 4
-      g.add 7
-      g.add 2
-      g.score.must_equal 18
-      g.score_for_frame(1).must_equal 9
-      g.score_for_frame(2).must_equal 18
-      g.current_frame.must_equal 3
-    end
+  it 'four throws no mark' do
+    g.add 5
+    g.add 4
+    g.add 7
+    g.add 2
+    g.score.must_equal 18
+    g.score_for_frame(1).must_equal 9
+    g.score_for_frame(2).must_equal 18
+    g.current_frame.must_equal 3
   end
 
-  context "scoring a spare" do
-    it 'returns the correct score' do
-      g.add 3
-      g.add 7
-      g.add 3
-      g.score_for_frame(1).must_equal 13
-      g.current_frame.must_equal 2
-    end
+  it 'simple spare' do
+    g.add 3
+    g.add 7
+    g.add 3
+    g.score_for_frame(1).must_equal 13
+    g.current_frame.must_equal 2
   end
 
-  context "score of frame after a spare" do
-    it 'returns the correct score' do
-      g.add 3
-      g.add 7
-      g.add 3
-      g.add 2
-      g.score_for_frame(1).must_equal 13
-      g.score_for_frame(2).must_equal 18
-      g.score.must_equal 18
-      g.current_frame.must_equal 3
-    end
+  it 'simple frame after spare' do
+    g.add 3
+    g.add 7
+    g.add 3
+    g.add 2
+    g.score_for_frame(1).must_equal 13
+    g.score_for_frame(2).must_equal 18
+    g.score.must_equal 18
+    g.current_frame.must_equal 3
   end
 
-  context "simple strike" do
-    it 'returns the correct score' do
-      g.add 10
-      g.add 3
-      g.add 6
-      g.score_for_frame(1).must_equal 19
-      g.score.must_equal 28
-      g.current_frame.must_equal 3
-    end
+  it 'simple strike' do
+    g.add 10
+    g.add 3
+    g.add 6
+    g.score_for_frame(1).must_equal 19
+    g.score.must_equal 28
+    g.current_frame.must_equal 3
   end
 
-  context "perfect game" do
-    it 'returns the correct score' do
-      12.times { g.add 10 }
-      g.score.must_equal 300
-      g.current_frame.must_equal 11
-    end
+  it 'perfect game' do
+    12.times { g.add 10 }
+    g.score.must_equal 300
+    g.current_frame.must_equal 11
   end
 
-  context "test end of array" do
-    it 'returns the correct score' do
-      9.times { g.add 0 }
-      g.add 2
-      g.add 8 # spare on 10th frame
-      g.add 10 # strike in last position of array
-      g.score.must_equal 20
-    end
+  it 'end of array' do
+    9.times { g.add 0 }
+    g.add 2
+    g.add 8 # spare on 10th frame
+    g.add 10 # strike in last position of array
+    g.score.must_equal 20
   end
 
 end
