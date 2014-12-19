c1dd5b290f75f5a7ac0bec3fe33b8fc505b21df7
Ricardo Cruz <ricardocruz.a89@gmail.com>
2014-12-19 05:23:10 +0000

    changed game spec to use let instead of subject

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,50 +2,59 @@ require 'spec_helper'
 require 'game'
 
 describe Game do
-  subject { Game.new }
+  let(:g) { Game.new }
 
   context "with two throws" do
     it 'returns the correct result' do
-      subject.add 5
-      subject.add 4
-      subject.score.must_equal 9
-      subject.current_frame.must_equal 2
+      g.add 5
+      g.add 4
+      g.score.must_equal 9
+      g.current_frame.must_equal 2
     end
   end
 
   context "4 throws" do
     it 'returns the score of the requested frame' do
-      subject.add 5
-      subject.add 4
-      subject.add 7
-      subject.add 2
-      subject.score.must_equal 18
-      subject.score_for_frame(1).must_equal 9
-      subject.score_for_frame(2).must_equal 18
-      subject.current_frame.must_equal 3
+      g.add 5
+      g.add 4
+      g.add 7
+      g.add 2
+      g.score.must_equal 18
+      g.score_for_frame(1).must_equal 9
+      g.score_for_frame(2).must_equal 18
+      g.current_frame.must_equal 3
     end
   end
 
   context "scoring a spare" do
     it 'returns the correct score' do
-      subject.add 3
-      subject.add 7
-      subject.add 3
-      subject.score_for_frame(1).must_equal 13
-      subject.current_frame.must_equal 2
+      g.add 3
+      g.add 7
+      g.add 3
+      g.score_for_frame(1).must_equal 13
+      g.current_frame.must_equal 2
     end
   end
 
   context "score of frame after a spare" do
     it 'returns the correct score' do
-      subject.add 3
-      subject.add 7
-      subject.add 3
-      subject.add 2
-      subject.score_for_frame(1).must_equal 13
-      subject.score_for_frame(2).must_equal 18
-      subject.score.must_equal 18
-      subject.current_frame.must_equal 3
+      g.add 3
+      g.add 7
+      g.add 3
+      g.add 2
+      g.score_for_frame(1).must_equal 13
+      g.score_for_frame(2).must_equal 18
+      g.score.must_equal 18
+      g.current_frame.must_equal 3
+    end
+  end
+
+  context "simple strike" do
+    it 'returns the correct score' do
+      g.add 10
+      g.add 3
+      g.add 6
+      g.score_for_frame(1).must_equal 19
     end
   end
 
