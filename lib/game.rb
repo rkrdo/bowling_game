75f21e17f8ab1e64ca16cd8d24bd7b1ba6c41284
Ricardo Cruz <ricardocruz.a89@gmail.com>
2014-12-20 05:50:17 +0000

    Updated Game class to use the Scorer to calculate the score

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,12 @@
+require 'scorer'
+
 class Game
 
   def initialize
     @score = 0
-    @throws = Array.new(21) { 0 }
-    @current_throw = 0
     @current_frame = 1
     @first_throw_in_frame = true
+    @scorer = Scorer.new
   end
 
   def score
@@ -17,27 +18,13 @@ class Game
   end
 
   def add(pins)
-    @throws[@current_throw += 1] = pins
+    @scorer.add_throw pins
     @score += pins
     adjust_current_frame(pins)
   end
 
   def score_for_frame(the_frame)
-    score = 0
-    @ball = 0
-    the_frame.times do |current_frame|
-      if strike?
-        score += 10 + next_two_balls_for_strike
-        @ball += 1
-      elsif spare?
-        score += 10 + next_ball_for_spare
-        @ball += 2
-      else
-        score += two_balls_in_frame
-        @ball += 2
-      end
-    end
-    score
+    @scorer.score_for_frame the_frame
   end
 
   private
@@ -54,26 +41,6 @@ class Game
       @current_frame += 1
     end
     @current_frame = [11 , @current_frame].min
-  end
-
-  def strike?
-   @throws[@ball + 1] == 10
-  end
-
-  def spare?
-    (@throws[@ball + 1] + @throws[@ball + 2]) == 10
-  end
-
-  def next_two_balls_for_strike
-    (@throws[@ball + 2] + @throws[@ball + 3])
-  end
-
-  def next_ball_for_spare
-    @throws[@ball + 3]
-  end
-
-  def two_balls_in_frame
-    @throws[@ball + 1] + @throws[@ball + 2]
   end
 
 end
