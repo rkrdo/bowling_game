ab2fe95df62457f803b2768ddb96d8ed451f9757
Ricardo Cruz <ricardocruz.a89@gmail.com>
2015-04-13 15:19:21 -0500

    Initial setup for JS implementation

---
+++ js/gulpfile.js
@@ -0,0 +1,18 @@
+/*  https://github.com/gulpjs/gulp/blob/master/docs/recipes/mocha-test-runner-with-gulp.md */
+
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+
+gulp.task('default', function() {
+  return gulp.src(['test/*.js'], { read: false })
+  .pipe(mocha({
+    reporter: 'spec',
+    globals: {
+      should: require('chai').assert
+    }
+  }));
+});
+
+gulp.task('watch-mocha', function() {
+  gulp.watch(['lib/**', 'test/**'], ['mocha']);
+});
