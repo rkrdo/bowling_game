c370c6f17ec97e92b412a0ecf7e58c851722d6d4
Ricardo Cruz <ricardocruz.a89@gmail.com>
2015-06-09 17:53:06 -0500

    Added test for four throws

--- js/test/game_test.js
+++ js/test/game_test.js
@@ -19,4 +19,16 @@ describe('Game', function() {
     });
   });
 
+  describe('test four throws', function() {
+    it('returns the correct score for a frame', function() {
+      let g = new Game();
+      g.add(5);
+      g.add(4);
+      g.add(7);
+      g.add(2);
+      assert.equal(18, g.score);
+      assert.equal(9, g.scoreForFrame(1));
+      assert.equal(18, g.scoreForFrame(2));
+    });
+  });
 });
