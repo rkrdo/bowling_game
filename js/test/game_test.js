9606c62d87025c3fe1390452dceaecaa87a75747
Ricardo Cruz <ricardocruz.a89@gmail.com>
2015-06-09 17:52:50 -0500

    Added test for two throws

--- js/test/game_test.js
+++ js/test/game_test.js
@@ -4,9 +4,19 @@ var Game = require('../lib/game');
 describe('Game', function() {
   describe('test one throw', function() {
     it('returns the correct score', function() {
-      var g = new Game();
+      let g = new Game();
       g.add(5);
       assert.equal(5, g.score);
     });
   });
+
+  describe('test two throws', function() {
+    it('returns the correct score', function() {
+      let g = new Game();
+      g.add(5);
+      g.add(4);
+      assert.equal(9, g.score);
+    });
+  });
+
 });
