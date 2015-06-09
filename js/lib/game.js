ab99f947d45bb4050c799804e7233a290b6e42c2
Ricardo Cruz <ricardocruz.a89@gmail.com>
2015-06-09 18:39:47 -0500

    Added test for scoreForFrame

--- js/lib/game.js
+++ js/lib/game.js
@@ -1,10 +1,25 @@
 class Game {
   constructor() {
-    this.score = 0;
+    this._itsScore = 0;
+    this._itsThrows = new Array(21);
+    this._itsCurrentThrow = 0;
+  }
+
+  get score() {
+    return this._itsScore;
   }
 
   add(pins) {
-    this.score += pins;
+    this._itsThrows[this._itsCurrentThrow++] = pins;
+    this._itsScore += pins;
+  }
+
+  scoreForFrame(frame) {
+    let score = 0;
+    for(let ball = 0; frame > 0 && ( ball < this._itsCurrentThrow); ball +=2, frame--) {
+      score += this._itsThrows[ball] + this._itsThrows[ball + 1];
+    }
+    return score;
   }
 
 }
